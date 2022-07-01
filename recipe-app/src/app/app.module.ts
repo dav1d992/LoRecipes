9e13455e1ce14dbf6f3d567e213abc9ff2ee27c2
dav1d992 <daviden@live.dk>
2022-07-01 21:48:10 +0200

    Added basic ng app with bootstrap

---
+++ recipe-app/src/app/app.module.ts
@@ -0,0 +1,25 @@
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
+import { CommonModule } from '@angular/common';
+
+import { AppComponent } from './app.component';
+import { ServerComponent } from './server/server.component';
+import { ServersComponent } from './servers/servers.component';
+
+
+@NgModule({
+  declarations: [					
+    AppComponent,
+      ServerComponent,
+      ServersComponent,
+   ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    CommonModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
